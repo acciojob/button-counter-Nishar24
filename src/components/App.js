a9814e71744f5375ebbf5a030ab5d935ff4322f8
Nishar24 <mdnishar75640@gmail.com>
2025-03-23 02:55:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+       <p>Button clicked {count} times</p>
+       <button onClick={onAdd}>Click me</button>
     </div>
   )
 }
